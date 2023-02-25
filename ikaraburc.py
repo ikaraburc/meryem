0070ec415367271e91b87641bd73a5c773156b22
ikaraburc <97704774+ikaraburc@users.noreply.github.com>
2023-02-25 16:11:31 +0300

    Update ikaraburc.py

--- ikaraburc.py
+++ ikaraburc.py
@@ -513,7 +513,7 @@ class coin_trader:
         headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
 
         url = '/spot/my_trades'
-        tt = int( time time() - 30 * 24 * 60 * 60 )
+        tt = int(time.time() - 20 * 24 * 60 * 60 )
         query_param = 'currency_pair=' + self.coin + "&from=" + str(tt)
 
         sign_headers = gen_sign('GET', prefix + url, query_param)
