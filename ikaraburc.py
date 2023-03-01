4a244e5983a6c4c8bd0a7f6d40c5ccd6cd45816a
ikaraburc <97704774+ikaraburc@users.noreply.github.com>
2023-03-02 00:15:23 +0300

    Update ikaraburc.py

--- ikaraburc.py
+++ ikaraburc.py
@@ -702,12 +702,12 @@ while True:
     if adk >= 1.15:
         bolge = "USYükseliş..."
         asi, afi, ma = 4, 7, 4
-        alk, slk = 5, 2
+        alk, slk = 5, 1
 
     elif 1.15 > adk >= 1.10:
         bolge = "SYükseliş..."
         asi, afi, ma = 3, 7, 3
-        alk, slk = 5, 3
+        alk, slk = 5, 2
 
     elif 1.10 > adk >= 1.05:
         bolge = "Yükseliş..."
